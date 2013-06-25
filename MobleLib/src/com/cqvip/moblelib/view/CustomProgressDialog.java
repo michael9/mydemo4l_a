db5002e3716e555be63b0232b16c4a1efc244088
ling <421620330@qq.com>
2013-06-25 14:33:00 +0800

    自定义进度条2

---
+++ MobleLib/src/com/cqvip/moblelib/view/CustomProgressDialog.java
@@ -0,0 +1,113 @@
+package com.cqvip.moblelib.view;
+
+/**************************************************************************************
+* [Project]
+*       MyProgressDialog
+* [Package]
+*       com.lxd.widgets
+* [FileName]
+*       CustomProgressDialog.java
+* [Copyright]
+*       Copyright 2012 LXD All Rights Reserved.
+* [History]
+*       Version          Date              Author                        Record
+*--------------------------------------------------------------------------------------
+*       1.0.0           2012-4-27         lxd (rohsuton@gmail.com)        Create
+**************************************************************************************/	
+
+import com.cqvip.moblelib.R;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
+import android.view.Gravity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+/********************************************************************
+ * [Summary]
+ *       TODO 请在此处简要描述此类所实现的功能。因为这项注释主要是为了在IDE环境中生成tip帮助，务必简明扼要
+ * [Remarks]
+ *       TODO 请在此处详细描述类的功能、调用方法、注意事项、以及与其它类的关系.
+ *******************************************************************/
+
+public class CustomProgressDialog extends Dialog {
+	private Context context = null;
+	private static CustomProgressDialog customProgressDialog = null;
+	
+	public CustomProgressDialog(Context context){
+		super(context);
+		this.context = context;
+	}
+	
+	public CustomProgressDialog(Context context, int theme) {
+        super(context, theme);
+        this.context = context;
+    }
+	
+	public static CustomProgressDialog createDialog(Context context){
+		customProgressDialog = new CustomProgressDialog(context,R.style.CustomProgressDialog);
+		customProgressDialog.setContentView(R.layout.customprogressdialog);
+		customProgressDialog.getWindow().getAttributes().gravity = Gravity.CENTER;
+		
+		return customProgressDialog;
+	}
+	private ImageView imageView;
+    public void onWindowFocusChanged(boolean hasFocus){
+    	
+    	if (customProgressDialog == null){
+    		return;
+    	}
+    	
+    	Animation operatingAnim = AnimationUtils.loadAnimation(context, R.anim.loadingrotate);
+    	LinearInterpolator lin = new LinearInterpolator();
+    	operatingAnim.setInterpolator(lin);
+    	
+         imageView = (ImageView) customProgressDialog.findViewById(R.id.loadingImageView);
+       // AnimationDrawable animationDrawable = (AnimationDrawable) imageView.getBackground();
+        if (operatingAnim != null) {
+        	imageView.startAnimation(operatingAnim);
+        }
+       // animationDrawable.start();
+    }
+ 
+    @Override
+    public void dismiss() {
+    	// TODO Auto-generated method stub
+    	super.dismiss();
+    	imageView.clearAnimation();
+    }
+    /**
+     * 
+     * [Summary]
+     *       setTitile 标题
+     * @param strTitle
+     * @return
+     *
+     */
+    public CustomProgressDialog setTitile(String strTitle){
+    	return customProgressDialog;
+    }
+    
+    /**
+     * 
+     * [Summary]
+     *       setMessage 提示内容
+     * @param strMessage
+     * @return
+     *
+     */
+    public CustomProgressDialog setMessage(String strMessage){
+    	TextView tvMsg = (TextView)customProgressDialog.findViewById(R.id.id_tv_loadingmsg);
+    	
+    	if (tvMsg != null){
+    		tvMsg.setText(strMessage);
+    	}
+    	
+    	return customProgressDialog;
+    }
+}
