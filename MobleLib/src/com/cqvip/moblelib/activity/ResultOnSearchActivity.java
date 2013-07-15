d6c5cd7012a6733c33fc0b3fc5a51f6e77a0fd35
michael carlos <michael82carlos@gmail.com>
2013-07-15 16:39:25 +0800

    馆藏查询

--- MobleLib/src/com/cqvip/moblelib/activity/ResultOnSearchActivity.java
+++ MobleLib/src/com/cqvip/moblelib/activity/ResultOnSearchActivity.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.json.JSONObject;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -23,6 +25,11 @@ import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.android.volley.VolleyError;
+import com.android.volley.Request.Method;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
+import com.android.volley.toolbox.StringRequest;
 import com.cqvip.moblelib.R;
 import com.cqvip.moblelib.adapter.BookAdapter;
 import com.cqvip.moblelib.base.IBookManagerActivity;
@@ -34,8 +41,9 @@ import com.cqvip.moblelib.model.Result;
 import com.cqvip.moblelib.view.CustomProgressDialog;
 import com.cqvip.utils.Tool;
 
-public class ResultOnSearchActivity extends BaseImageActivity implements IBookManagerActivity,OnItemClickListener {
-	
+public class ResultOnSearchActivity extends BaseImageActivity implements
+		 OnItemClickListener {
+
 	public static final int GETFIRSTPAGE = 1;
 	public static final int GETNEXTPAGE = 2;
 
@@ -50,116 +58,197 @@ public class ResultOnSearchActivity extends BaseImageActivity implements IBookMa
 	private View moreprocess;
 	private RelativeLayout noResult_rl;
 	private View title_bar;
+	private Map<String, String> gparams;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_result_on_search);
 		context = this;
-		imgsearch = (ImageButton)findViewById(R.id.search_seach_btn);
-		edit = (EditText)findViewById(R.id.search_et);
-		listview = (ListView)findViewById(R.id.search_res_lv);
-		listview.setOnItemClickListener((OnItemClickListener)this);
-		ManagerService.allActivity.add(this);
-		customProgressDialog=CustomProgressDialog.createDialog(this);
+		imgsearch = (ImageButton) findViewById(R.id.search_seach_btn);
+		edit = (EditText) findViewById(R.id.search_et);
+		listview = (ListView) findViewById(R.id.search_res_lv);
+		listview.setOnItemClickListener((OnItemClickListener) this);
+		customProgressDialog = CustomProgressDialog.createDialog(this);
 		noResult_rl = (RelativeLayout) findViewById(R.id.noresult_rl);
 		edit.setText(getIntent().getStringExtra("ISBN"));
-		
-		imgsearch.setOnClickListener(new View.OnClickListener() {
-				
-				@Override
-				public void onClick(View v) {
-					hideKeybord();
-					key = edit.getText().toString().trim();
-					if(!Tool.checkNetWork(context)){
-						return ;
-					}
-					if(TextUtils.isEmpty(key)){
-						Tool.ShowMessages(context, "请输入关键字");
-						return;
-					}
-					//判断是否是isbn号查询
-					page = 1;
-					if(Tool.isbnMatch(key)){
-					getHomePage(key,GETFIRSTPAGE,DEFAULT_COUNT,GETFIRSTPAGE,GlobleData.QUERY_ISBN);
-					}else{
-					getHomePage(key,GETFIRSTPAGE,DEFAULT_COUNT,GETFIRSTPAGE,GlobleData.QUERY_ALL);
-					}
-//					Tool.ShowMessages(context, "开始搜索");
-//					progressDialog.show();
-				}
 
-			});
-		
-          
-		  edit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-				
-			  @Override
-				public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-					if(TextUtils.isEmpty(edit.getText().toString())){
-						return true;
-					}
-					key = edit.getText().toString().trim();
-					//隐藏键盘
-					hideKeybord();
-					//检查0网络
-					if(!Tool.checkNetWork(context)){
-						return false;
-					}
-					//网络访问,获取首页
-					page = 1;
-					if(Tool.isbnMatch(key)){
-						getHomePage(key,GETFIRSTPAGE,DEFAULT_COUNT,GETFIRSTPAGE,GlobleData.QUERY_ISBN);
-						}else{
-						getHomePage(key,GETFIRSTPAGE,DEFAULT_COUNT,GETFIRSTPAGE,GlobleData.QUERY_ALL);
-						}
+		imgsearch.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				hideKeybord();
+				key = edit.getText().toString().trim();
+				if (!Tool.checkNetWork(context)) {
+					return;
+				}
+				if (TextUtils.isEmpty(key)) {
+					Tool.ShowMessages(context, "请输入关键字");
+					return;
+				}
+				// 判断是否是isbn号查询
+				page = 1;
+				if (Tool.isbnMatch(key)) {
+					getHomePage(key, GETFIRSTPAGE, DEFAULT_COUNT, GETFIRSTPAGE,
+							GlobleData.QUERY_ISBN);
+				} else {
+					getHomePage(key, GETFIRSTPAGE, DEFAULT_COUNT, GETFIRSTPAGE,
+							GlobleData.QUERY_ALL);
+				}
+			}
+
+		});
+
+		edit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+			@Override
+			public boolean onEditorAction(TextView v, int actionId,
+					KeyEvent event) {
+				if (TextUtils.isEmpty(edit.getText().toString())) {
 					return true;
 				}
-
-			});
-	
-		  title_bar=findViewById(R.id.head_bar);
-			TextView title = (TextView)title_bar.findViewById(R.id.txt_header);
-			title.setText(R.string.main_search);
-			ImageView back = (ImageView)title_bar.findViewById(R.id.img_back_header);
-			back.setOnClickListener(new View.OnClickListener() {
-				
-				@Override
-				public void onClick(View v) {
-					finish();
+				key = edit.getText().toString().trim();
+				// 隐藏键盘
+				hideKeybord();
+				// 检查0网络
+				if (!Tool.checkNetWork(context)) {
+					return false;
 				}
-			});
-	
+				// 网络访问,获取首页
+				page = 1;
+				if (Tool.isbnMatch(key)) {
+					getHomePage(key, GETFIRSTPAGE, DEFAULT_COUNT, GETFIRSTPAGE,
+							GlobleData.QUERY_ISBN);
+				} else {
+					getHomePage(key, GETFIRSTPAGE, DEFAULT_COUNT, GETFIRSTPAGE,
+							GlobleData.QUERY_ALL);
+				}
+				return true;
+			}
+
+		});
+
+		title_bar = findViewById(R.id.head_bar);
+		TextView title = (TextView) title_bar.findViewById(R.id.txt_header);
+		title.setText(R.string.main_search);
+		ImageView back = (ImageView) title_bar
+				.findViewById(R.id.img_back_header);
+		back.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+
 	}
+
 	/**
 	 * 隐藏键盘
 	 */
 	private void hideKeybord() {
-		InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-		if(imm.isActive()){
+		InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+		if (imm.isActive()) {
 			imm.hideSoftInputFromWindow(edit.getWindowToken(), 0);
 		}
 	}
+
+	Listener<String> backlistener = new Listener<String>() {
+		@Override
+		public void onResponse(String response) {
+			// TODO Auto-generated method stub
+			customProgressDialog.dismiss();
+			try {
+				// JSONObject mj=new JSONObject(response);
+				List<Book> lists = Book.formList(response);
+				if (lists != null && !lists.isEmpty()) {
+					listview.setVisibility(View.VISIBLE);
+					noResult_rl.setVisibility(View.GONE);
+					adapter = new BookAdapter(context, lists, mImageFetcher);
+					listview.setAdapter(adapter);
+				} else {
+					listview.setVisibility(View.GONE);
+					noResult_rl.setVisibility(View.VISIBLE);
+				}
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+
+		}
+	};
+
+	Listener<String> backlistenermore = new Listener<String>() {
+		@Override
+		public void onResponse(String response) {
+			// TODO Auto-generated method stub
+			customProgressDialog.dismiss();
+			moreprocess.setVisibility(View.GONE);
+			try {
+				// JSONObject mj=new JSONObject(response);
+				List<Book> lists = Book.formList(response);
+				if (lists != null && !lists.isEmpty()) {
+					adapter.addMoreData(lists);
+				} else {
+					Tool.ShowMessages(context, "没有更多内容可供加载");
+				}
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+
+		}
+	};
+
+	ErrorListener el = new ErrorListener() {
+		@Override
+		public void onErrorResponse(VolleyError arg0) {
+			// TODO Auto-generated method stub
+			customProgressDialog.dismiss();
+
+		}
+	};
+
+	private void requestVolley(String addr, Listener<String> bl, int method) {
+		try {
+			StringRequest mys = new StringRequest(method, addr, bl, el) {
+
+				protected Map<String, String> getParams()
+						throws com.android.volley.AuthFailureError {
+					return gparams;
+				};
+			};
+			mQueue.add(mys);
+			mQueue.start();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
 	/**
 	 * 请求网络，获取数据
+	 * 
 	 * @param key
 	 * @param page
 	 * @param count
 	 */
-	private void getHomePage(String key,int page ,int count,int type,String field) {
+	private void getHomePage(String key, int page, int count, int type,
+			String field) {
 		customProgressDialog.show();
-		HashMap map=new HashMap();
-		map.put("key", key);
-		map.put("page", page);
-		map.put("count", count);
-		map.put("library",GlobleData.SZLG_LIB_ID);
-		map.put("field",field);
-		Task tsHome;
-		if(type == GETFIRSTPAGE){
-		 tsHome=new Task(Task.TASK_QUERY_BOOK,map);
-		}else{
-		 tsHome=new Task(Task.TASK_QUERY_MORE,map);
+		gparams = new HashMap<String, String>();
+		gparams.put("keyword", key);
+		gparams.put("curpage", "" + page);
+		gparams.put("perpage", "" + count);
+		gparams.put("library", GlobleData.SZLG_LIB_ID);
+		gparams.put("field", field);
+
+		if (type == GETFIRSTPAGE) {
+			requestVolley(GlobleData.SERVER_URL
+					+ "/library/bookquery/search.aspx", backlistener,
+					Method.POST);
+		} else {
+			requestVolley(GlobleData.SERVER_URL
+					+ "/library/bookquery/search.aspx", backlistenermore,
+					Method.POST);
 		}
-		ManagerService.addNewTask(tsHome);
 	}
 
 	@Override
@@ -168,85 +257,43 @@ public class ResultOnSearchActivity extends BaseImageActivity implements IBookMa
 		getMenuInflater().inflate(R.menu.result_on_search, menu);
 		return true;
 	}
-	@Override
-	public void init() {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void refresh(Object... obj) {
-		//显示
-		customProgressDialog.dismiss();
-		hideKeybord();
-		int type = (Integer)obj[0];
-		 
-		List<Book> lists = (List<Book>)obj[1];
-		if(type == GETFIRSTPAGE ){
-			if(lists!=null&&!lists.isEmpty()){
-				listview.setVisibility(View.VISIBLE);
-				noResult_rl.setVisibility(View.GONE);
-			adapter = new BookAdapter(context,lists,mImageFetcher);
-			listview.setAdapter(adapter);
-			
-		}
-		else
-		{
-			//Tool.ShowMessages(context, "不好意思，没有找到你想找的资源！");
-			//listview.setAdapter(new SimpleAdapter(context, getData(), android.R.layout.simple_list_item_1, new String[]{"no"}, new int[] { android.R.id.text1 }));
-			listview.setVisibility(View.GONE);
-			noResult_rl.setVisibility(View.VISIBLE);
-			
-		}
-		}else if(type == GETNEXTPAGE){
-			moreprocess.setVisibility(View.GONE);
-			if(lists!=null&&!lists.isEmpty()){
-				adapter.addMoreData(lists);
-				}else{
-					Tool.ShowMessages(context, "没有更多内容可供加载");
-				}
-		}
-	}
-	private List<? extends Map<String, ?>> getData() {
-		List<Map<String,String>> list = new ArrayList<Map<String,String>>();
-		
-			Map<String,String> map = new HashMap<String,String>();
-			map.put("no","查询无记录");
-			list.add(map);
-        return list;
-	}
+
+
 	@Override
 	public void onItemClick(AdapterView<?> arg0, View arg1, int positon, long id) {
-		Log.i("item","===============click=");
-		if (id == -2) //更多
+		Log.i("item", "===============click=");
+		if (id == -2) // 更多
 		{
-			//进度条
+			// 进度条
 			moreprocess = arg1.findViewById(R.id.footer_progress);
 			moreprocess.setVisibility(View.VISIBLE);
-			//请求网络更多
-			if(Tool.isbnMatch(key)){
-				getHomePage(key,page+1,DEFAULT_COUNT,GETNEXTPAGE,GlobleData.QUERY_ISBN);
-				}else{
-				getHomePage(key,page+1,DEFAULT_COUNT,GETNEXTPAGE,GlobleData.QUERY_ALL);
-				}
-			page = page+1;
-		}else{
+			// 请求网络更多
+			if (Tool.isbnMatch(key)) {
+				getHomePage(key, page + 1, DEFAULT_COUNT, GETNEXTPAGE,
+						GlobleData.QUERY_ISBN);
+			} else {
+				getHomePage(key, page + 1, DEFAULT_COUNT, GETNEXTPAGE,
+						GlobleData.QUERY_ALL);
+			}
+			page = page + 1;
+		} else {
 			Book book = adapter.getLists().get(positon);
-			if(book!=null){
-				Log.i("ResultOnSearchActivity",book.toString());
-				Intent _intent = new Intent(context,DetailBookActivity.class);
+			if (book != null) {
+				Log.i("ResultOnSearchActivity", book.toString());
+				Intent _intent = new Intent(context, DetailBookActivity.class);
 				Bundle bundle = new Bundle();
 				bundle.putSerializable("book", book);
 				_intent.putExtra("detaiinfo", bundle);
 				startActivity(_intent);
 			}
-			
-//			Book book =  lists.get(position-1);
-//			if(book!=null){
-//			Bundle bundle = new Bundle();
-//			bundle.putSerializable("book", book);
-//			_intent.putExtra("detaiinfo", bundle);
-//			startActivityForResult(_intent, 1);
-			}
+
+			// Book book = lists.get(position-1);
+			// if(book!=null){
+			// Bundle bundle = new Bundle();
+			// bundle.putSerializable("book", book);
+			// _intent.putExtra("detaiinfo", bundle);
+			// startActivityForResult(_intent, 1);
 		}
-		
+	}
+
 }
