f692f6b543168b52a653bfdb52ca63ac3b0bae69
linghp <421620330@qq.com>
2013-08-19 15:59:28 +0800

    电子书分类

--- MobleLib/src/com/cqvip/moblelib/activity/EBookActiviy.java
+++ MobleLib/src/com/cqvip/moblelib/activity/EBookActiviy.java
@@ -3,11 +3,15 @@ package com.cqvip.moblelib.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CheckedTextView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -17,87 +21,141 @@ import com.cqvip.moblelib.R;
 
 /**
  * <p>
- * 文件名称: EBookActiviy.java
- * 文件描述: 电子书
- * 版权所有: 版权所有(C)2013-2020
- * 公          司: 重庆维普咨询有限公司
- * 内容摘要: 
- * 其他说明:
- * 完成日期： 201年5月10日
- * 修改记录: 
+ * 文件名称: EBookActiviy.java 文件描述: 电子书 版权所有: 版权所有(C)2013-2020 公 司: 重庆维普咨询有限公司
+ * 内容摘要: 其他说明: 完成日期： 201年5月10日 修改记录:
  * </p>
  * 
  * @author LHP,LJ
  */
 public class EBookActiviy extends BaseActivity {
 
-//	 private static final String[] EBOOKTYPE = new String[] {
-//	        "馆藏书目", "维普期刊", "万方期刊","超星图书","内置图书"
-//	    };
-	private  String[] EBOOKTYPE;
+	// private static final String[] EBOOKTYPE = new String[] {
+	// "馆藏书目", "维普期刊", "万方期刊","超星图书","内置图书"
+	// };
+	private String[] EBOOKTYPE;
 	private ViewGroup searchbar;
+	private int[] drawableids = { R.drawable.sign_vip, R.drawable.sign_chaoxing,
+			R.drawable.sign_fangzheng };
+	private  int currentID=-1;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_ebook_activiy);
 		View v = findViewById(R.id.ebook_title);
-		TextView title = (TextView)v.findViewById(R.id.txt_header);
+		TextView title = (TextView) v.findViewById(R.id.txt_header);
 		title.setText(R.string.main_ebook);
-		ImageView back = (ImageView)v.findViewById(R.id.img_back_header);
+		ImageView back = (ImageView) v.findViewById(R.id.img_back_header);
 		back.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				finish();
-				
+
 			}
 		});
-		
-//		SearchView sc = (SearchView)findViewById(R.id.search_view);
+
+		// SearchView sc = (SearchView)findViewById(R.id.search_view);
 		EBOOKTYPE = getResources().getStringArray(R.array.ebooktype);
-		
-		ListView lv = (ListView)findViewById(R.id.ebook_search_list);
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_multiple_choice,EBOOKTYPE);
+
+		ListView lv = (ListView) findViewById(R.id.ebook_search_list);
+		final MyAdapter  adapter = new MyAdapter(this, EBOOKTYPE, drawableids);
 		lv.setAdapter(adapter);
-		lv.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-		lv.setItemsCanFocus(false);
-		lv.setItemChecked(0, true);	
-		//lv.setEnabled(false);
-		lv.setFocusable(false);
-		
+
 		lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
-				
-				startActivity(new Intent(EBookActiviy.this,PeriodicalClassfyActivity.class));
-				
+				if(position==0){
+					adapter.setCurrentID(position);
+					adapter.notifyDataSetChanged();
+					startActivity(new Intent(EBookActiviy.this,
+					PeriodicalClassfyActivity.class));
+				}
 			}
-		
+
 		});
-		
-		searchbar=(ViewGroup)findViewById(R.id.searchbar);
+
+		searchbar = (ViewGroup) findViewById(R.id.searchbar);
 		searchbar.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Intent intent=new Intent(EBookActiviy.this, EBookSearchActivity.class);
+				Intent intent = new Intent(EBookActiviy.this,
+						EBookSearchActivity.class);
 				startActivity(intent);
 				finish();
 			}
 		});
+
+		final EditText et = (EditText) findViewById(R.id.ebook_edit);
+		et.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+				imm.hideSoftInputFromWindow(et.getWindowToken(), 0);
+				Intent intent = new Intent(EBookActiviy.this,
+						EBookSearchActivity.class);
+				startActivity(intent);
+				finish();
+			}
+		});
+	}
+
+	public class MyAdapter extends BaseAdapter {
+		private String[] eBookTypes;
+		private Context context;
+		private int[] drawableids;
+		int  currentID=-1;
+
+		public MyAdapter(Context context, String[] eBookTypes, int[] drawableids) {
+			this.eBookTypes = eBookTypes;
+			this.context = context;
+			this.drawableids = drawableids;
+		}
+
+		public void setCurrentID(int currentID) {
+			this.currentID = currentID;
+		}
 		
-		 final EditText et = (EditText)findViewById(R.id.ebook_edit);
-		    et.setOnClickListener(new View.OnClickListener() {
-				
-				@Override
-				public void onClick(View v) {
-					InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-					imm.hideSoftInputFromWindow(et.getWindowToken(), 0);
-					Intent intent = new Intent(EBookActiviy.this,EBookSearchActivity.class) ;
-					startActivity(intent);
-					finish();
-				}
-			});
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return eBookTypes.length;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return position;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			LayoutInflater inflater;
+			if (convertView == null) {
+				inflater = LayoutInflater.from(context);
+				convertView = inflater.inflate(R.layout.item_ebooktype, null);
+			}
+			ImageView iv = (ImageView) convertView
+					.findViewById(R.id.booktype_img);
+			CheckedTextView checkedTextView = (CheckedTextView) convertView
+					.findViewById(android.R.id.text1);
+			iv.setImageResource(drawableids[position]);
+			checkedTextView.setText(eBookTypes[position]);
+			  if(position==this.currentID)
+				  checkedTextView.setChecked(true);
+			  else
+				  checkedTextView.setChecked(false);
+			return convertView;
+		}
+
 	}
 }
