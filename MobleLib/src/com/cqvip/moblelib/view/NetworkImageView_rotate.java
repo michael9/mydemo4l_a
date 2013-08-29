ac677aea812fff740276ead3fdac2185bea3ce42
linghp <421620330@qq.com>
2013-08-29 17:04:38 +0800

    动画

--- MobleLib/src/com/cqvip/moblelib/view/NetworkImageView_rotate.java
+++ MobleLib/src/com/cqvip/moblelib/view/NetworkImageView_rotate.java
@@ -41,189 +41,218 @@ import com.cqvip.moblelib.utils.Rotate3dAnimation;
  * associated request.
  */
 public class NetworkImageView_rotate extends ImageView {
-    /** The URL of the network image to load */
-    private String mUrl;
+	/** The URL of the network image to load */
+	private String mUrl;
 
-    /**
-     * Resource ID of the image to be used as a placeholder until the network image is loaded.
-     */
-    private int mDefaultImageId;
+	/**
+	 * Resource ID of the image to be used as a placeholder until the network
+	 * image is loaded.
+	 */
+	private int mDefaultImageId;
 
-    /**
-     * Resource ID of the image to be used if the network response fails.
-     */
-    private int mErrorImageId;
+	/**
+	 * Resource ID of the image to be used if the network response fails.
+	 */
+	private int mErrorImageId;
 
-    /** Local copy of the ImageLoader. */
-    private ImageLoader mImageLoader;
+	/** Local copy of the ImageLoader. */
+	private ImageLoader mImageLoader;
 
-    /** Current ImageContainer. (either in-flight or finished) */
-    private ImageContainer mImageContainer;
-    
+	/** Current ImageContainer. (either in-flight or finished) */
+	private ImageContainer mImageContainer;
+
 	private ViewGroup mContainer;
 	private ImageView readerinfo_f_lay, readerinfo_b_lay;
 
-    public NetworkImageView_rotate(Context context) {
-        this(context, null);
-    }
+	public NetworkImageView_rotate(Context context) {
+		this(context, null);
+	}
 
-    public NetworkImageView_rotate(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
+	public NetworkImageView_rotate(Context context, AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
 
-    public NetworkImageView_rotate(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-    }
+	public NetworkImageView_rotate(Context context, AttributeSet attrs,
+			int defStyle) {
+		super(context, attrs, defStyle);
+	}
 
 	/**
-     * Sets URL of the image that should be loaded into this view. Note that calling this will
-     * immediately either set the cached image (if available) or the default image specified by
-     * {@link NetworkImageView_rotate#setDefaultImageResId(int)} on the view.
-     *
-     * NOTE: If applicable, {@link NetworkImageView_rotate#setDefaultImageResId(int)} and
-     * {@link NetworkImageView_rotate#setErrorImageResId(int)} should be called prior to calling
-     * this function.
-     *
-     * @param url The URL that should be loaded into this ImageView.
-     * @param imageLoader ImageLoader that will be used to make the request.
-     */
-    public void setImageUrl(String url, ImageLoader imageLoader) {
-        mUrl = url;
-        mImageLoader = imageLoader;
-        // The URL has potentially changed. See if we need to load it.
-        loadImageIfNecessary(false);
-    }
+	 * Sets URL of the image that should be loaded into this view. Note that
+	 * calling this will immediately either set the cached image (if available)
+	 * or the default image specified by
+	 * {@link NetworkImageView_rotate#setDefaultImageResId(int)} on the view.
+	 * 
+	 * NOTE: If applicable,
+	 * {@link NetworkImageView_rotate#setDefaultImageResId(int)} and
+	 * {@link NetworkImageView_rotate#setErrorImageResId(int)} should be called
+	 * prior to calling this function.
+	 * 
+	 * @param url
+	 *            The URL that should be loaded into this ImageView.
+	 * @param imageLoader
+	 *            ImageLoader that will be used to make the request.
+	 */
+	public void setImageUrl(String url, ImageLoader imageLoader) {
+		mUrl = url;
+		mImageLoader = imageLoader;
+		// The URL has potentially changed. See if we need to load it.
+		loadImageIfNecessary(false);
+	}
 
-    /**
-     * Sets the default image resource ID to be used for this view until the attempt to load it
-     * completes.
-     */
-    public void setDefaultImageResId(int defaultImage) {
-        mDefaultImageId = defaultImage;
-    }
+	/**
+	 * Sets the default image resource ID to be used for this view until the
+	 * attempt to load it completes.
+	 */
+	public void setDefaultImageResId(int defaultImage) {
+		mDefaultImageId = defaultImage;
+	}
 
-    /**
-     * Sets the error image resource ID to be used for this view in the event that the image
-     * requested fails to load.
-     */
-    public void setErrorImageResId(int errorImage) {
-        mErrorImageId = errorImage;
-    }
+	/**
+	 * Sets the error image resource ID to be used for this view in the event
+	 * that the image requested fails to load.
+	 */
+	public void setErrorImageResId(int errorImage) {
+		mErrorImageId = errorImage;
+	}
 
-    /**
-     * Loads the image for the view if it isn't already loaded.
-     * @param isInLayoutPass True if this was invoked from a layout pass, false otherwise.
-     */
-    private void loadImageIfNecessary(final boolean isInLayoutPass) {
-        int width = getWidth();
-        int height = getHeight();
+	/**
+	 * Loads the image for the view if it isn't already loaded.
+	 * 
+	 * @param isInLayoutPass
+	 *            True if this was invoked from a layout pass, false otherwise.
+	 */
+	private void loadImageIfNecessary(final boolean isInLayoutPass) {
+		int width = getWidth();
+		int height = getHeight();
 
-        boolean isFullyWrapContent = getLayoutParams() != null
-                && getLayoutParams().height == LayoutParams.WRAP_CONTENT
-                && getLayoutParams().width == LayoutParams.WRAP_CONTENT;
-        // if the view's bounds aren't known yet, and this is not a wrap-content/wrap-content
-        // view, hold off on loading the image.
-        if (width == 0 && height == 0 && !isFullyWrapContent) {
-            return;
-        }
+		boolean isFullyWrapContent = getLayoutParams() != null
+				&& getLayoutParams().height == LayoutParams.WRAP_CONTENT
+				&& getLayoutParams().width == LayoutParams.WRAP_CONTENT;
+		// if the view's bounds aren't known yet, and this is not a
+		// wrap-content/wrap-content
+		// view, hold off on loading the image.
+		if (width == 0 && height == 0 && !isFullyWrapContent) {
+			return;
+		}
 
-        // if the URL to be loaded in this view is empty, cancel any old requests and clear the
-        // currently loaded image.
-        if (TextUtils.isEmpty(mUrl)) {
-            if (mImageContainer != null) {
-                mImageContainer.cancelRequest();
-                mImageContainer = null;
-            }
-            //setImageBitmap(null);
-            return;
-        }
+		// if the URL to be loaded in this view is empty, cancel any old
+		// requests and clear the
+		// currently loaded image.
+		if (TextUtils.isEmpty(mUrl)) {
+			if (mImageContainer != null) {
+				mImageContainer.cancelRequest();
+				mImageContainer = null;
+			}
+			// setImageBitmap(null);
+			return;
+		}
 
-        // if there was an old request in this view, check if it needs to be canceled.
-        if (mImageContainer != null && mImageContainer.getRequestUrl() != null) {
-            if (mImageContainer.getRequestUrl().equals(mUrl)) {
-                // if the request is from the same URL, return.
-                return;
-            } else {
-                // if there is a pre-existing request, cancel it if it's fetching a different URL.
-                mImageContainer.cancelRequest();
-                setImageBitmap(null);
-            }
-        }
+		// if there was an old request in this view, check if it needs to be
+		// canceled.
+		if (mImageContainer != null && mImageContainer.getRequestUrl() != null) {
+			if (mImageContainer.getRequestUrl().equals(mUrl)) {
+				// if the request is from the same URL, return.
+				return;
+			} else {
+				// if there is a pre-existing request, cancel it if it's
+				// fetching a different URL.
+				mImageContainer.cancelRequest();
+				setImageBitmap(null);
+			}
+		}
 
-        // The pre-existing content of this view didn't match the current URL. Load the new image
-        // from the network.
-        ImageContainer newContainer = mImageLoader.get(mUrl,
-                new ImageListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        if (mErrorImageId != 0) {
-                            setImageResource(mErrorImageId);
-                        }
-                    }
+		// The pre-existing content of this view didn't match the current URL.
+		// Load the new image
+		// from the network.
+		ImageContainer newContainer = mImageLoader.get(mUrl,
+				new ImageListener() {
+					@Override
+					public void onErrorResponse(VolleyError error) {
+						if (mErrorImageId != 0) {
+							setImageResource(mErrorImageId);
+						}
+					}
 
-                    @Override
-                    public void onResponse(final ImageContainer response, boolean isImmediate) {
-                        // If this was an immediate response that was delivered inside of a layout
-                        // pass do not set the image immediately as it will trigger a requestLayout
-                        // inside of a layout. Instead, defer setting the image by posting back to
-                        // the main thread.
-                        if (isImmediate && isInLayoutPass) {
-                            post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    onResponse(response, false);
-                                }
-                            });
-                            return;
-                        }
+					@Override
+					public void onResponse(final ImageContainer response,
+							boolean isImmediate) {
+						// If this was an immediate response that was delivered
+						// inside of a layout
+						// pass do not set the image immediately as it will
+						// trigger a requestLayout
+						// inside of a layout. Instead, defer setting the image
+						// by posting back to
+						// the main thread.
+						if (isImmediate && isInLayoutPass) {
+							post(new Runnable() {
+								@Override
+								public void run() {
+									onResponse(response, false);
+								}
+							});
+							return;
+						}
 
-                        if (response.getBitmap() != null) {
-                        	//Log.i("NetworkImageView_rotate", "response.getBitmap()");
-                        	mContainer=(ViewGroup) getParent();
-                        	readerinfo_f_lay=(ImageView) mContainer.findViewById(R.id.defaut_book_img);
-                        	readerinfo_b_lay=NetworkImageView_rotate.this;
-                            setImageBitmap(response.getBitmap());
-                            applyRotation(0, 0, 90);
-                        } else if (mDefaultImageId != 0) {
-                            setImageResource(mDefaultImageId);
-                        }
-                    }
-                });
+						if (response.getBitmap() != null) {
+							// Log.i("NetworkImageView_rotate",
+							// "response.getBitmap()");
+							mContainer = (ViewGroup) getParent();
+							if (mContainer != null) {
+								readerinfo_f_lay = (ImageView) mContainer
+										.findViewById(R.id.defaut_book_img);
+							}
+							readerinfo_b_lay = NetworkImageView_rotate.this;
+							setImageBitmap(response.getBitmap());
+							applyRotation(0, 0, 90);
+						} else if (mDefaultImageId != 0) {
+							setImageResource(mDefaultImageId);
+						}
+					}
+				});
 
-        // update the ImageContainer to be the new bitmap container.
-        mImageContainer = newContainer;
-    }
+		// update the ImageContainer to be the new bitmap container.
+		mImageContainer = newContainer;
+	}
 
-    @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        super.onLayout(changed, left, top, right, bottom);
-        loadImageIfNecessary(true);
-    }
+	@Override
+	protected void onLayout(boolean changed, int left, int top, int right,
+			int bottom) {
+		super.onLayout(changed, left, top, right, bottom);
+		loadImageIfNecessary(true);
+	}
 
-    @Override
-    protected void onDetachedFromWindow() {
-        if (mImageContainer != null) {
-            // If the view was bound to an image request, cancel it and clear
-            // out the image from the view.
-            mImageContainer.cancelRequest();
-            setImageBitmap(null);
-            // also clear out the container so we can reload the image if necessary.
-            mImageContainer = null;
-        }
-        super.onDetachedFromWindow();
-    }
+	@Override
+	protected void onDetachedFromWindow() {
+		if (mImageContainer != null) {
+			// If the view was bound to an image request, cancel it and clear
+			// out the image from the view.
+			mImageContainer.cancelRequest();
+			setImageBitmap(null);
+			// also clear out the container so we can reload the image if
+			// necessary.
+			mImageContainer = null;
+		}
+		this.clearAnimation();
+		super.onDetachedFromWindow();
+	}
 
-    @Override
-    protected void drawableStateChanged() {
-        super.drawableStateChanged();
-        invalidate();
-    }
-    
-    
+	@Override
+	protected void drawableStateChanged() {
+		super.drawableStateChanged();
+		invalidate();
+	}
+
 	private void applyRotation(int position, float start, float end) {
 		// Find the center of the container
-		final float centerX = mContainer.getWidth() / 2.0f;
-		final float centerY = mContainer.getHeight() / 2.0f;
+		final float centerX, centerY;
+		if (mContainer != null && readerinfo_f_lay != null) {
+			centerX = mContainer.getWidth() / 2.0f;
+			centerY = mContainer.getHeight() / 2.0f;
+		} else {
+			centerX = getWidth() / 2.0f;
+			centerY = getHeight() / 2.0f;
+		}
 
 		// Create a new 3D rotation with the supplied parameter
 		// The animation listener is used to trigger the next animation
@@ -234,7 +263,11 @@ public class NetworkImageView_rotate extends ImageView {
 		rotation.setInterpolator(new LinearInterpolator());
 		rotation.setAnimationListener(new DisplayNextView(position));
 
-		mContainer.startAnimation(rotation);
+		if (mContainer != null && readerinfo_f_lay != null) {
+			mContainer.startAnimation(rotation);
+		} else {
+			readerinfo_b_lay.startAnimation(rotation);
+		}
 	}
 
 	/**
@@ -253,7 +286,11 @@ public class NetworkImageView_rotate extends ImageView {
 		}
 
 		public void onAnimationEnd(Animation animation) {
+			if (mContainer != null && readerinfo_f_lay != null) {
 			mContainer.post(new SwapViews(mPosition));
+			}else{
+				post(new SwapViews(mPosition));
+			}
 		}
 
 		public void onAnimationRepeat(Animation animation) {
@@ -272,12 +309,19 @@ public class NetworkImageView_rotate extends ImageView {
 		}
 
 		public void run() {
-			final float centerX = mContainer.getWidth() / 2.0f;
-			final float centerY = mContainer.getHeight() / 2.0f;
+			final float centerX, centerY;
+			if (mContainer != null && readerinfo_f_lay != null) {
+				centerX = mContainer.getWidth() / 2.0f;
+				centerY = mContainer.getHeight() / 2.0f;
+			} else {
+				centerX = getWidth() / 2.0f;
+				centerY = getHeight() / 2.0f;
+			}
 			Rotate3dAnimation rotation;
 
 			if (mPosition > -1) {
-				readerinfo_f_lay.setVisibility(View.GONE);
+				if (readerinfo_f_lay != null)
+					readerinfo_f_lay.setVisibility(View.GONE);
 				readerinfo_b_lay.setVisibility(View.VISIBLE);
 				readerinfo_b_lay.requestFocus();
 
@@ -285,17 +329,22 @@ public class NetworkImageView_rotate extends ImageView {
 						0.0f, false);
 			} else {
 				readerinfo_b_lay.setVisibility(View.GONE);
-				readerinfo_f_lay.setVisibility(View.VISIBLE);
-				readerinfo_f_lay.requestFocus();
-				rotation = new Rotate3dAnimation(90, 0, centerX, centerY,
-						0.0f, false);
+				if (readerinfo_f_lay != null) {
+					readerinfo_f_lay.setVisibility(View.VISIBLE);
+					readerinfo_f_lay.requestFocus();
+				}
+				rotation = new Rotate3dAnimation(90, 0, centerX, centerY, 0.0f,
+						false);
 			}
 
 			rotation.setDuration(200);
 			rotation.setFillAfter(true);
 			rotation.setInterpolator(new LinearInterpolator());
-
-			mContainer.startAnimation(rotation);
+			if (mContainer != null && readerinfo_f_lay != null) {
+				mContainer.startAnimation(rotation);
+			} else {
+				readerinfo_b_lay.startAnimation(rotation);
+			}
 		}
 	}
 }
