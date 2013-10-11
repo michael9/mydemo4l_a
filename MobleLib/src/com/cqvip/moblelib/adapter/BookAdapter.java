56eb5df151f198cd9b3d9dea20b48dd01db2253c
linghp <421620330@qq.com>
2013-10-11 15:06:06 +0800

    馆藏图片显示不对，重用imageview所致

--- MobleLib/src/com/cqvip/moblelib/adapter/BookAdapter.java
+++ MobleLib/src/com/cqvip/moblelib/adapter/BookAdapter.java
@@ -128,11 +128,14 @@ public class BookAdapter extends BaseAdapter {
 		holder.u_abstract.setText(describe + book.getU_abstract());
 
 		String url=book.getCover_path();
-        if(!TextUtils.isEmpty(url)){
+		Log.i("BookAdapter", position+":"+url);
+		holder.img.setPosition(position);
+        //if(!TextUtils.isEmpty(url)){
         	holder.img.setImageUrl(url, mImageLoader);
-        } else {
-            holder.img.setImageResource(R.drawable.defaut_book);
-        }
+//        } else {
+//        	Log.i("BookAdapter01", position+":"+url);
+//            //holder.img.setImageResource(R.drawable.defaut_book);
+//        }
 		return convertView;
 	}
 
