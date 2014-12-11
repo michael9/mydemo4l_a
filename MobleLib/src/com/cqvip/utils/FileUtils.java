e21b3264ab5357382ad6490d914f3bbef453ee03
soldierguard <luojiang515@gmail.com>
2014-12-11 16:48:54 +0800

    修改入馆指南

--- MobleLib/src/com/cqvip/utils/FileUtils.java
+++ MobleLib/src/com/cqvip/utils/FileUtils.java
@@ -1,6 +1,10 @@
 package com.cqvip.utils;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
+
+import android.content.Context;
 
 public class FileUtils {
 	
@@ -31,4 +35,20 @@ public class FileUtils {
 	    }   
 	    return flag;   
 	}  
+	
+	
+	   public static String getFromAssets(Context context,String fileName){
+           try {
+                InputStreamReader inputReader = new InputStreamReader( context.getResources().getAssets().open(fileName) );
+               BufferedReader bufReader = new BufferedReader(inputReader);
+               String line="";
+               String Result="";
+               while((line = bufReader.readLine()) != null)
+                   Result += line;
+               return Result;
+           } catch (Exception e) {
+               e.printStackTrace();
+           }
+           return null;
+   }
 }
